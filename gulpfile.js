197f1f38fefd4f12f6677f8b3fc4fef514ae3e76
Thomas Scheibitz <hello@scheibitz.com>
2018-05-15 22:10:47 +0200

    [TASK] remove unused gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uncss = require('gulp-uncss');
 var exec = require('child_process').exec;
-const KssSection = require('kss').KssSection;
 
 gulp.task('sass', function() {
 	return gulp.src('kss_styleguide/scheibo-template/kss-assets/**/*.scss')
@@ -27,20 +26,3 @@ gulp.task('watch', ['sass', 'kss'], function() {
 	gulp.watch('kss_styleguide/scheibo-template/kss-assets/**/*.scss', ['sass']);
 	gulp.watch('kss-scheibo/**/**', ['kss']);
 });
-
-gulp.task('teste', function() {
-	console.log(KssSection);
-});
-
-// var replace = require('gulp-replace');
-//
-// gulp.task('templates', function(){
-// 	gulp.src(['kss-scheibo/**/*.html'])
-// 		.pipe(replace(/<sg\-insert>([0-9\.]*)(.*)<\/sg\-insert>/, function(match, p1, offset, string) {
-// 			// Replace foobaz with barbaz and log a ton of information
-// 			// See http://mdn.io/string.replace#Specifying_a_function_as_a_parameter
-// 			console.log('Found ' + match + ' with param ' + p1 + ' at ' + offset + ' inside of ' + string);
-// 			return 'bar' + p1;
-// 		}))
-// 		.pipe(gulp.dest('build/'));
-// });
