d61f67b3cf6b508ac16d906e5151ba6edd884561
Thomas Scheibitz <hello@scheibitz.com>
2018-05-16 20:09:43 +0200

    [TASK] refactor codestyle

--- kss_styleguide/scheibo-template/builder.js
+++ kss_styleguide/scheibo-template/builder.js
@@ -1,3 +1,4 @@
+/* jshint node: true */
 'use strict';
 
 let KssBuilderHandlebars = require('kss/builder/handlebars');
@@ -6,11 +7,11 @@ class KssBuilderScheibo extends KssBuilderHandlebars {
 
 	prepare(styleGuide) {
 		this.setupEachSection(styleGuide);
-		return super.prepare(styleGuide)
+		return super.prepare(styleGuide);
 	}
 
 	setupEachSection(styleGuide) {
-		styleGuide.sections().forEach(function (section) {
+		styleGuide.sections().forEach(function(section) {
 			let markup = section.data.markup;
 			let markupMatch = markup.match(/<insert-markup>(.*?)<\/insert-markup>/i);
 
@@ -18,8 +19,10 @@ class KssBuilderScheibo extends KssBuilderHandlebars {
 			let rawMatch = raw.match(/<insert-markup>(.*?)<\/insert-markup>/i);
 
 			if (markupMatch && raw) {
-				section.data.markup = markup.replace(/<insert-markup>[\s\S]*?<\/insert-markup>/g, styleGuide.sections(markupMatch[1]).data.markup);
-				section.meta.raw = raw.replace(/<insert-markup>[\s\S]*?<\/insert-markup>/g, styleGuide.sections(rawMatch[1]).meta.raw);
+				section.data.markup = markup.replace(/<insert-markup>[\s\S]*?<\/insert-markup>/g,
+					styleGuide.sections(markupMatch[1]).data.markup);
+				section.meta.raw = raw.replace(/<insert-markup>[\s\S]*?<\/insert-markup>/g,
+					styleGuide.sections(rawMatch[1]).meta.raw);
 			}
 		});
 	}
