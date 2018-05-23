8e25d8e583ba844f081069adae2c2930ac0c75a9
Thomas Scheibitz <thomas.scheibitz@in2code.de>
2018-05-23 08:16:39 +0200

    [TASK] replace gulp-uncss with postcss-uncss / uncss / gulp-postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,25 @@
 /* jshint node: true */
 'use strict';
 
-var fs = require('fs');
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var uncss = require('gulp-uncss');
-var exec = require('child_process').exec;
-var settings = JSON.parse(fs.readFileSync('./kss-scheibo.json'));
+let fs = require('fs');
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let postcss = require('gulp-postcss');
+let uncss = require('postcss-uncss');
+let exec = require('child_process').exec;
+let settings = JSON.parse(fs.readFileSync('./kss-scheibo.json'));
 
 gulp.task('sass', function() {
+	let plugins = [
+		uncss({
+			html: ['docs/**/*.html'],
+			ignore: ['.kss-guides-mode .kss-modifier__example']
+		}),
+	];
+
 	return gulp.src('kss_styleguide/scheibo-template/kss-assets/**/*.scss')
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-		.pipe(uncss({
-			html: ['kss_styleguide/scheibo-template/**/*.html'],
-			ignore: ['.kss-guides-mode .kss-modifier__example']
-		}))
+		.pipe(postcss(plugins))
 		.pipe(gulp.dest('kss_styleguide/scheibo-template/kss-assets/'));
 });
 
