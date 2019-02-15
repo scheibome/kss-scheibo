606a1335191983561e125f6cca9d1269af8fc52a
Thomas Scheibitz <hello@scheibitz.com>
2019-02-15 20:13:18 +0100

    [TASK] add bodyclass into example in listview

--- kss_styleguide/scheibo-template/builder.js
+++ kss_styleguide/scheibo-template/builder.js
@@ -7,6 +7,7 @@ class KssBuilderScheibo extends KssBuilderHandlebars {
 	prepare(styleGuide) {
 		return super.prepare(styleGuide).then(styleGuide => {
 			require('../../lib/modules/colors')(this.Handlebars);
+			require('../../lib/modules/modifierBodyClass')(this.Handlebars);
 			require('../../lib/modules/modifierInsertCode')(this.Handlebars);
 			require('../../lib/modules/modifierFullscreen')(this.Handlebars, this.options);
 			require('../../lib/modules/modifierInsertSection')(this.Handlebars);
