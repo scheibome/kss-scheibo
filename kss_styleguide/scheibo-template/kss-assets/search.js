364a2941cf345e150a700e5e605a5b1deb5fc1c0
scheibome <hello@scheibitz.com>
2022-01-10 18:17:49 +0100

    [BUGFIX] fix the search

--- kss_styleguide/scheibo-template/kss-assets/search.js
+++ kss_styleguide/scheibo-template/kss-assets/search.js
@@ -3,7 +3,16 @@
 	var searchInput = document.getElementById('kss-search__input');
 	var searchList = document.getElementById('kss-search__list');
 	var searchItems = document.querySelectorAll('.kss-search__item');
+	var mainNavigation = document.querySelector('nav.kss-nav');
 	var searchListDisplayClassname = 'kss-search__list--display';
+
+	function recursiveListSearch(el) {
+		var nextListItem = el.parentNode.parentNode;
+		if (nextListItem.nodeName === 'LI') {
+			nextListItem.style.display = '';
+			recursiveListSearch(nextListItem);
+		}
+	}
 
 	function kssSearch() {
 		var filter;
@@ -16,6 +25,7 @@
 			a = searchItems[i].getElementsByTagName('a')[0];
 			if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
 				searchItems[i].style.display = '';
+				recursiveListSearch(searchItems[i]);
 			} else {
 				searchItems[i].style.display = 'none';
 			}
@@ -23,12 +33,17 @@
 	}
 	document.addEventListener('click', function(e) {
 		if (e.target === searchInput) {
-			searchList.classList.add(searchListDisplayClassname);
 			searchInput.addEventListener('keyup', function() {
+				if (!searchList.classList.contains(searchListDisplayClassname)) {
+					searchList.classList.add(searchListDisplayClassname);
+					mainNavigation.style.opacity = '0';
+					console.log(1641834680659, mainNavigation);
+				}
 				kssSearch();
 			});
 		} else {
 			searchList.classList.remove(searchListDisplayClassname);
+			mainNavigation.style.opacity = '1';
 		}
 	});
 })();
